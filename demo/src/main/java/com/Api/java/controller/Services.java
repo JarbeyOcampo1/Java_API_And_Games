17a02dc704b5c9c01b197c5cf002f7466c2b09e8
JarbeyOcampo1 <164644585+JarbeyOcampo1@users.noreply.github.com>
2025-01-30 12:46:47 -0500

    ADD GAMES

--- demo/src/main/java/com/Api/java/controller/Services.java
+++ demo/src/main/java/com/Api/java/controller/Services.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,22 +14,19 @@ import com.Api.java.model.Game;
 @Service
 public class Services {
 
-    private static final String FILE_PATH = "games.json";  // Solo el nombre del archivo
+    private static final String FILE_PATH = "games.json";
     private final ObjectMapper objectMapper = new ObjectMapper();
     
     // Método para obtener todos los juegos
     public List<Game> get_all_games() {
         try {
-            // Acceder al archivo usando ClassPathResource
             File file = new ClassPathResource(FILE_PATH).getFile();
-            
-            // Si el archivo no existe, lo crea y lo inicializa con una lista vacía
+    
             if (!file.exists()) {
                 file.createNewFile();
                 objectMapper.writeValue(file, new ArrayList<Game>());
             }
-            
-            // Leer el archivo JSON y convertirlo a un arreglo de objetos Game
+
             Game[] gamesArray = objectMapper.readValue(file, Game[].class);
             return new ArrayList<>(Arrays.asList(gamesArray));
             
